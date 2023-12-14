2f80353f0c5f08e1dacba0945e7f22ffa8440c6a
Gabriel Bruno <114627827+GabrielBruno7@users.noreply.github.com>
2023-12-13 23:09:06 -0300

    Variaveis

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+
+$data = [
+    'name' => 'Gabriel',
+    'age' => 12, 
+    'city' => 'Minas Gerais'
+];
+
+echo $data['name'].PHP_EOL;
+
+
+define('pi', 3.14 );
+echo constant('pi');
+
+?>
